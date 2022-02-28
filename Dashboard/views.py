a40d14ddf57247f8a29acf49e4553d0fa66512ac
Anuja <anujanikumbh5@GMAIL.COM>
2022-02-28 11:45:24 +0530

    created notes function and added path of notes.html file

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,5 +1,9 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request,'Dashboard/home.html')       #rendering home file
+
+def notes(request):
+    return render(request,'Dashboard/notes.html')      #rendering notes file
