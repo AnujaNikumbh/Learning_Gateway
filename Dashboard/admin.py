c3b4509056a845afefe9b4d98b17ead24d08ee04
Anuja <anujanikumbh5@GMAIL.COM>
2022-02-28 12:15:21 +0530

    registered notes model in admin site

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+admin.site.register(Notes)     #registering notes model       #django admin admin adds extra s in every table so it will be display as notess
