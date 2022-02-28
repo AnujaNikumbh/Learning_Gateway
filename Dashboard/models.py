a4c96c82d7c8c2fa98c09f12b6fb1c41d8696552
Anuja <anujanikumbh5@GMAIL.COM>
2022-02-28 12:16:02 +0530

    created notes table

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Notes(models.Model):   #class notes containning below 3 columns
+    user = models.ForeignKey(User,on_delete=models.CASCADE)   #when this user is deleted then this notes also deleted from database
+    title = models.CharField(max_length=200)
+    description = models.TextField()
