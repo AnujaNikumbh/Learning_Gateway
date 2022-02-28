0dded0e94aed4da61f5a8fe8686ef9153cfa0e92
Anuja <anujanikumbh5@GMAIL.COM>
2022-02-28 11:43:48 +0530

    added path to the views.notes function

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views                 #importing views in current application using dot
 
 urlpatterns =[
-    path('',views.home)             #mapping views.home function 
+    path('',views.home, name="home") ,            #mapping views.home function 
+    path('notes',views.notes,name="notes")       #mapping views.notes function
 ]
