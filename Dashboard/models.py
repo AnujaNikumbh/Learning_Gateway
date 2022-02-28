1915d4f268f8e31b7a1fea1edc6ff4faf059603b
Anuja <anujanikumbh5@GMAIL.COM>
2022-02-28 12:16:41 +0530

    created notes table

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -6,3 +6,4 @@ class Notes(models.Model):   #class notes containning below 3 columns
     user = models.ForeignKey(User,on_delete=models.CASCADE)   #when this user is deleted then this notes also deleted from database
     title = models.CharField(max_length=200)
     description = models.TextField()
+    
