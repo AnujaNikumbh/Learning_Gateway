27fbb6b5333a4d3542b4968cc426e9cb8d997ff1
Anuja <anujanikumbh5@GMAIL.COM>
2022-03-11 19:24:36 +0530

    imported forms and created object

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,12 +1,13 @@
 import re
 from django.shortcuts import render
-from . models import Notes           # importing notes model 
+from . forms import *       # importing notes model 
 
 # Create your views here.
 def home(request):
     return render(request,'Dashboard/home.html')       #rendering home file
 
 def notes(request):
+    form = NotesForm()
     notes = Notes.objects.filter(user=request.user)   #created notes object and pass request user that is login user
     context = {'notes':notes}         #passing notes object using context
     return render(request,'Dashboard/notes.html',context)      #rendering notes file  #passing context object in file
