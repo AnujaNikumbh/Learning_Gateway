287e21babb359bcf7a57d630961e2cd3838a76a2
Anuja <anujanikumbh5@GMAIL.COM>
2022-03-12 12:15:35 +0530

    added object notesform

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -7,7 +7,7 @@ def home(request):
     return render(request,'Dashboard/home.html')       #rendering home file
 
 def notes(request):
-    form = NotesForm()
+    form = NotesForm()  #object
     notes = Notes.objects.filter(user=request.user)   #created notes object and pass request user that is login user
     context = {'notes':notes,'form':form}         #passing notes object using context
     return render(request,'Dashboard/notes.html',context)      #rendering notes file  #passing context object in file
