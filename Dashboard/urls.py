d8637cd33526fa30a40ce11b55e573c46c1a008c
Anuja <anujanikumbh5@GMAIL.COM>
2022-03-12 13:14:06 +0530

    added delete_note function path

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views                 #importing views in current application usin
 
 urlpatterns =[
     path('',views.home, name="home") ,            #mapping views.home function 
-    path('notes',views.notes,name="notes")       #mapping views.notes function
+    path('notes',views.notes,name="notes"),       #mapping views.notes function
+    path('delete_note/<int:pk>', views.delete_note, name="delete-note"),  #mapping views.delete_note function
 ]
