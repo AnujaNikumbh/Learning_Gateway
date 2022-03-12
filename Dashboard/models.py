8977b9aaf394650d6894a8a81b949a3b921d33fd
Anuja <anujanikumbh5@GMAIL.COM>
2022-03-12 14:05:47 +0530

    created model for homework

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -13,3 +13,16 @@ class Notes(models.Model):   #class notes containning below 3 columns
     class Meta:         #for removing that extra s added by django admin
         verbose_name ="notes"    #new name
         verbose_name_plural = "notes"   #previous name
+        
+        
+
+class Homework(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=50)
+    title = models.CharField(max_length=100) 
+    description = models.TextField()       
+    due = models.DateTimeField()
+    is_finished = models.BooleanField(default=False)
+    
+    def __str__(self):  # to display title name
+        return self.title
