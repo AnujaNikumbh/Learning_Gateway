2759543e0e7d87cd35cf0cf2bcc52ae29a238f3f
Anuja <anujanikumbh5@GMAIL.COM>
2022-03-12 12:40:19 +0530

    added post method and imported messages in the views

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,13 +1,25 @@
 import re
 from django.shortcuts import render
 from . forms import *       # importing forms model 
+from django.contrib import messages #imported messages
+
+
 
 # Create your views here.
 def home(request):
     return render(request,'Dashboard/home.html')       #rendering home file
 
 def notes(request):
-    form = NotesForm()  #object
+    if request.method == 'POST':     #added post method
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            notes= Notes(user=request.user,title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+        messages.success(request,f"Notes Added from {request.user.username} Successfully!")    
+        
+    else: 
+        form = NotesForm()
+    
     notes = Notes.objects.filter(user=request.user)   #created notes object and pass request user that is login user
     context = {'notes':notes,'form':form}         #passing notes object using context
     return render(request,'Dashboard/notes.html',context)      #rendering notes file  #passing context object in file
