59affc8a0dfeb49fe5302613133c78ecc1fc5dc8
Anuja <anujanikumbh5@GMAIL.COM>
2022-03-12 14:05:18 +0530

    added homework functions url

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -5,5 +5,7 @@ urlpatterns =[
     path('',views.home, name="home") ,            #mapping views.home function 
     path('notes',views.notes,name="notes"),       #mapping views.notes function
     path('delete_note/<int:pk>', views.delete_note, name="delete-note"),#mapping views.delete_note function
-    path('notes_detail/<int:pk>', views.NotesDetailView.as_view(), name="notes-detail")
+    path('notes_detail/<int:pk>', views.NotesDetailView.as_view(), name="notes-detail"),#mapping views.notes_detail function
+    
+    path('homework', views.homework, name="homework"),
 ]
