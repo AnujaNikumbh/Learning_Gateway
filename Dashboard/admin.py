d0cdf3081f6c8783829c9a9974d126d1a8ecda75
Anuja <anujanikumbh5@GMAIL.COM>
2022-03-16 19:51:06 +0530

    update

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from . models import *
 
 # Register your models here.
-admin.site.register(Notes) #registering notes model       #django admin admin adds extra s in every table so it will be display as notess
+admin.site.register(Notes) #registering notes model       #django admin adds extra s in every table so it will be display as notess
 admin.site.register(Homework)
