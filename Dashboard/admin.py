06e4b564c951d8d8ceeb70f9c8a504df8fe1ca6b
Anuja <anujanikumbh5@GMAIL.COM>
2022-03-21 18:47:30 +0530

    added new model Todo

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -4,3 +4,4 @@ from . models import *
 # Register your models here.
 admin.site.register(Notes) #registering notes model       #django admin adds extra s in every table so it will be display as notess
 admin.site.register(Homework)
+admin.site.register(Todo)
