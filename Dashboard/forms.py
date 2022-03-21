8ee67c54b41db5c846a8217fa5772d4f9f05b6fe
Anuja <anujanikumbh5@GMAIL.COM>
2022-03-21 18:23:46 +0530

    created class dashboardform

--- Dashboard/forms.py
+++ Dashboard/forms.py
@@ -20,6 +20,6 @@ class HomeworkForm(forms.ModelForm):
 
 
 class DashboardForm(forms.Form):
-    text = forms.CharField(max_length=100,label="Enter your search: ")
+    text = forms.CharField(max_length=100,label="Enter your search ")
             
         
