a237d269b40371c1a7b3ef2d9150a315986495cd
Anuja <anujanikumbh5@GMAIL.COM>
2022-03-31 09:23:43 +0530

    added success message with f string and method form.cleaned data

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -341,6 +341,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f"Account created for {username}!!")
+            #redirect("login")
     else:        
         form = UserRegistrationForm()
     context ={
